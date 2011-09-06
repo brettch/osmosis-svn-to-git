241633be3bb24aa1f34bcf380dcde0847d204a04
Brett Henderson <brett@bretth.com>
2011-09-06 13:48:07 +0000

    Now re-sequencing all 5 dump files.

--- src/com/bretth/osmosis/git_migration/RenumberRevisions.java
+++ src/com/bretth/osmosis/git_migration/RenumberRevisions.java
@@ -3,27 +3,32 @@ package com.bretth.osmosis.git_migration;
 import java.io.File;
 
 public class RenumberRevisions {
-
-	public static void main(String[] args) {
-		File inDumpFile;
-		File outDumpFile;
-		RevisionMapper revisionMapper;
+	
+	private static void renumberRevisions(RevisionMapper revisionMapper, String inDump, String outDump, String repository) {
 		LineWriter lineWriter;
 		RevisionRenumberTask renumberTask;
 		LineReader lineReader;
 		
-		inDumpFile = new File("../osmosis.dmp");
-		outDumpFile = new File("../test.dmp");
-		
-		revisionMapper = new RevisionMapper();
-		
-		lineWriter = new LineWriter(outDumpFile);
+		lineWriter = new LineWriter(new File(outDump));
 		renumberTask = new RevisionRenumberTask(lineWriter, revisionMapper, "osm");
-		lineReader = new LineReader(inDumpFile, renumberTask);
+		lineReader = new LineReader(new File(inDump), renumberTask);
 		
 		lineReader.run();
 		
 		lineReader.close();
 		lineWriter.close();
+	}
+	
+
+	public static void main(String[] args) {
+		RevisionMapper revisionMapper;
+		
+		revisionMapper = new RevisionMapper();
+
+		renumberRevisions(revisionMapper, "../1-bh.dmp", "../1-bh.fixed.dmp", "bh");
+		renumberRevisions(revisionMapper, "../2-bh.dmp", "../2-bh.fixed.dmp", "bh");
+		renumberRevisions(revisionMapper, "../3-bh.dmp", "../3-bh.fixed.dmp", "bh");
+		renumberRevisions(revisionMapper, "../4-osm.dmp", "../4-osm.fixed.dmp", "osm");
+		renumberRevisions(revisionMapper, "../5-osm.dmp", "../5-osm.fixed.dmp", "osm");
 	}
 }

ebbba46db25383472fb78738bc7bdbec543a5c01
Brett Henderson <brett@bretth.com>
2011-09-15 22:49:19 +1000

    Updated to retrieve SVN history into multiple repositories.

--- git-join-history/migrate_to_git.sh
+++ git-join-history/migrate_to_git.sh
@@ -7,20 +7,17 @@ cd data
 
 rm -rf *.git
 
-mkdir target.git
-cd target.git
-
-# Get the conduit part of the repo which existed up to revision 471, and name it "conduit"
+# Retrieve the conduit part of the repo which existed up to revision 471
+mkdir conduit.git
+cd conduit.git
 git svn init file://`pwd`/../bhmain-sync/conduit
-sed -i "s/svn-remote \"svn\"/svn-remote \"conduit\"/" .git/config
+git svn fetch -r417:471 -A "${USERS_FILE}"
+cd ..
 
-# Retrieve the conduit history
-git svn fetch -r417:471 -A "${USERS_FILE}" conduit
-
-# Point at the osmosis part of the repo which existed without trunk/tags/branches from revisions 427 to 474, and name it "bh_simple"
+# Retrieve the osmosis part of the repo which existed without trunk/tags/branches from revisions 427 to 474, and name it "bhsimple"
+mkdir bhsimple.git
+cd bhsimple.git
 git svn init file://`pwd`/../bhmain-sync/osmosis
-sed -i "s/svn-remote \"svn\"/svn-remote \"bh_simple\"/" .git/config
-
-# Retrieve the bh simple history
-git svn fetch -r473:474 -A "${USERS_FILE}" bh_simple
+git svn fetch -r473:474 -A "${USERS_FILE}"
+cd ..
 

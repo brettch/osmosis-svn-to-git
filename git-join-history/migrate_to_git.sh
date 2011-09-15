eea3e61e199f4e7187e3c47052e01598607a1843
Brett Henderson <brett@bretth.com>
2011-09-16 00:48:57 +1000

    Retrieve history for all repositories. Graft the remote branches together. Checkout the latest branch as master. Re-write the history and delete the grafts.

--- git-join-history/migrate_to_git.sh
+++ git-join-history/migrate_to_git.sh
@@ -1,10 +1,27 @@
 #!/bin/sh
 
+
+graft_branches() {
+	local PARENT=$1
+	local CHILD=$2
+
+	# Get the identifier of the last commit on the parent branch
+	# Looking for the first line like: commit <sha-commit-id>
+	local PARENT_LAST_COMMIT=$(git log ${PARENT}|grep "^commit "|head -1|cut -d " " -f 2)
+
+	# Get the identifier of the first commit on the bhsimple branch
+	# Looking for the last line like: commit <sha-commit-id>
+	local CHILD_FIRST_COMMIT=$(git log ${CHILD}|grep "^commit "|tail -1|cut -d " " -f 2)
+
+	# Create the graft entry
+	echo "${CHILD_FIRST_COMMIT} ${PARENT_LAST_COMMIT}" >> .git/info/grafts
+}
+
+
 USERS_FILE=`pwd`/../users.txt
 echo "${USERS_FILE}"
 
 cd data
-
 if false; then
 rm -rf *.git
 
@@ -29,6 +46,20 @@ git svn init -s file://`pwd`/../bhmain-sync/osmosis
 git svn fetch -r476:HEAD -A "${USERS_FILE}"
 cd ..
 
+# Retrieve the part of the repo which existed without trunk/tags/branches from revisions 4743 to 12410, and name it "osmsimple"
+mkdir osmsimple.git
+cd osmsimple.git
+git svn init file://`pwd`/../osm-sync/applications/utils/osmosis
+git svn fetch -r4743:12410 -A "${USERS_FILE}"
+cd ..
+
+# Retrieve the part of the repo which existed with trunk/tags/branches from revisions 12412 to HEAD, and name it "osmstdlayout"
+mkdir osmstdlayout.git
+cd osmstdlayout.git
+git svn init -s file://`pwd`/../osm-sync/applications/utils/osmosis
+git svn fetch -r12412:HEAD -A "${USERS_FILE}"
+cd ..
+
 fi
 
 # Create a target repository and add remotes to the other repos.
@@ -39,4 +70,27 @@ git init
 git remote add conduit ../conduit.git
 git remote add bhsimple ../bhsimple.git
 git remote add bhstdlayout ../bhstdlayout.git
+git remote add osmsimple ../osmsimple.git
+git remote add osmstdlayout ../osmstdlayout.git
+
+# Retrieve all data from the repositories
+git fetch conduit
+git fetch bhsimple
+git fetch bhstdlayout
+git fetch osmsimple
+git fetch osmstdlayout
+
+# Graft the branches together.
+graft_branches conduit/master bhsimple/master
+graft_branches bhsimple/master bhstdlayout/master
+graft_branches bhstdlayout/master osmsimple/master
+graft_branches osmsimple/master osmstdlayout/master
+
+# Build master off the last branch.
+git branch master osmstdlayout/master
+git checkout
+
+# Re-build the history based on the grafts file.
+git filter-branch
+rm .git/info/grafts
 

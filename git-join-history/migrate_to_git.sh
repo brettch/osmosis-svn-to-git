b0d69be13569413d75bfcb3c9d31336a29d2d72b
Brett Henderson <brett@bretth.com>
2011-09-21 00:22:30 +1000

    Added steps to re-graft tag history.
    Branch remote SVN tag branches to local branches to allow them to be modified.
    Grafted several tags with broken parents due to re-tagging in the SVN repo.

--- git-join-history/migrate_to_git.sh
+++ git-join-history/migrate_to_git.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 
 
+branch_svn_tags_to_local() {
+	git for-each-ref --format='%(refname)' 'refs/remotes/tags/*' | while read TAG_REFERENCE; do
+		# Get the bare tag name from the complete reference path
+		TAG_NAME=${TAG_REFERENCE#refs/remotes/tags/}
+		echo TAG_NAME: $TAG_NAME
+
+		git branch svntags/${TAG_NAME} $TAG_REFERENCE
+	done
+}
+
+
 graft_branches() {
 	local PARENT=$1
 	local CHILD=$2
@@ -30,6 +41,7 @@ mkdir conduit.git
 cd conduit.git
 git svn init file://`pwd`/../bhmain-sync/conduit
 git svn fetch -r417:471 -A "${USERS_FILE}"
+branch_svn_tags_to_local
 cd ..
 
 # Retrieve the osmosis part of the repo which existed without trunk/tags/branches from revisions 427 to 474, and name it "bhsimple"
@@ -37,6 +49,7 @@ mkdir bhsimple.git
 cd bhsimple.git
 git svn init file://`pwd`/../bhmain-sync/osmosis
 git svn fetch -r473:474 -A "${USERS_FILE}"
+branch_svn_tags_to_local
 cd ..
 
 # Retrieve the osmosis part of the repo which existed with trunk/tags/branches from revisions 476 to HEAD, and name it "bhstdlayout"
@@ -44,6 +57,14 @@ mkdir bhstdlayout.git
 cd bhstdlayout.git
 git svn init -s file://`pwd`/../bhmain-sync/osmosis
 git svn fetch -r476:HEAD -A "${USERS_FILE}"
+branch_svn_tags_to_local
+# Tag 0.4 was re-tagged and the parent commit of the final tag shows up as being the initial tag.
+# Reset the parent of the tag to be the correct trunk revision.
+echo "`git rev-parse refs/heads/svntags/0.4` `git rev-parse ":/Fixed the osmosis launch script to reflect the updated mysql jar file."`" >> .git/info/grafts
+git checkout svntags/0.4
+git filter-branch
+rm .git/info/grafts
+git checkout master
 cd ..
 
 # Retrieve the part of the repo which existed without trunk/tags/branches from revisions 4743 to 12410, and name it "osmsimple"
@@ -51,6 +72,7 @@ mkdir osmsimple.git
 cd osmsimple.git
 git svn init file://`pwd`/../osm-sync/applications/utils/osmosis
 git svn fetch -r4743:12410 -A "${USERS_FILE}"
+branch_svn_tags_to_local
 cd ..
 
 # Retrieve the part of the repo which existed with trunk/tags/branches from revisions 12412 to HEAD, and name it "osmstdlayout"
@@ -58,6 +80,15 @@ mkdir osmstdlayout.git
 cd osmstdlayout.git
 git svn init -s file://`pwd`/../osm-sync/applications/utils/osmosis
 git svn fetch -r12412:HEAD -A "${USERS_FILE}"
+branch_svn_tags_to_local
+# Tag 0.28 is missing its parent because it doesn't exist in this repository, we need to graft it in the final repository. (TODO: Fix in final repository)
+# Tag 0.32 was modified after creation to fix an artefact version in the maven POM.  That can't be changed now.
+# Tag 0.35.1 was re-tagged to fix the version number in ant and update changes.txt.
+echo "`git rev-parse refs/heads/svntags/0.35.1` `git rev-parse ":/Updated changes.txt with the fixes applied in this version."`" >> .git/info/grafts
+git checkout svntags/0.35.1
+git filter-branch
+rm .git/info/grafts
+git checkout master
 cd ..
 
 # Create a target repository and add remotes to the other repos.

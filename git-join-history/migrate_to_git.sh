3489c1854faf67d27906946defed65119687793a
Brett Henderson <brett@bretth.com>
2011-09-24 12:38:17 +1000

    Merge svn reference updates into main script.
    fix_svn_references has been merged into the migrate_to_git script.

--- git-join-history/migrate_to_git.sh
+++ git-join-history/migrate_to_git.sh
@@ -87,6 +87,54 @@ rewrite_branches() {
 }
 
 
+convert_svn_branches_to_tags() {
+	# Process all of the branches created from the SVN tags and convert to annotated tags
+	git for-each-ref --format='%(refname)' 'refs/heads/svntags/*' | while read TAG_REFERENCE; do
+		echo $TAG_REFERENCE
+
+		# Get the bare tag name from the complete reference path
+		TAG_NAME=${TAG_REFERENCE#refs/heads/svntags/}
+		echo "Processing tag : $TAG_NAME"
+
+		# Get the data that the branch object points to.  The ":" is followed by the
+		# path to retrieve which when left empty means the root.
+		DATA=$( git rev-parse "$TAG_REFERENCE": )
+
+		# Search back through the revision history to find the first commit that the
+		# data was created in its current state.  In other words, we want to strip off
+		# revisions that were used to create the tag path in SVN, but didn't modify
+		# the underlying data.
+		PARENT_REFERENCE="$TAG_REFERENCE";
+		echo PARENT_REFERENCE: $PARENT_REFERENCE
+		while [ $( git rev-parse --quiet --verify "$PARENT_REFERENCE"^: ) = "$DATA" ]; do
+			PARENT_REFERENCE="$PARENT_REFERENCE"^
+			echo PARENT_REFERENCE: $PARENT_REFERENCE
+		done
+		PARENT=$( git rev-parse "$PARENT_REFERENCE" )
+
+		# if this ancestor is in trunk then we can just tag it
+		# otherwise the tag has diverged from trunk and it's actually more like a
+		# branch than a tag
+		MERGE_BASE=$( git merge-base master $PARENT )
+		if [ ! "$MERGE_BASE" = "$PARENT" ]; then
+			echo "Check tag $TAG_NAME.  It appears to have been modified after branching from trunk, perhaps it was re-created and needs to be git grafted accordingly."
+		fi
+		TARGET_REFERENCE=$PARENT
+
+		# Create an annotated tag using the commit message used to create the SVN tag.
+		git show -s --pretty='format:%B' "$TAG_REFERENCE" | \
+			env \
+				GIT_COMMITTER_NAME="$(  git show -s --pretty='format:%an' "$TAG_REFERENCE" )" \
+				GIT_COMMITTER_EMAIL="$( git show -s --pretty='format:%ae' "$TAG_REFERENCE" )" \
+				GIT_COMMITTER_DATE="$(  git show -s --pretty='format:%ad' "$TAG_REFERENCE" )" \
+			git tag -a -F - "$TAG_NAME" "$TARGET_REFERENCE"
+
+		# Remove the now redundant tag branch.
+		git update-ref -d "$TAG_REFERENCE"
+	done
+}
+
+
 USERS_FILE=`pwd`/../users.txt
 echo "${USERS_FILE}"
 
@@ -247,6 +295,9 @@ git branch -d svnbranches/0.35-fixes
 # Re-build the history based on the grafts file.
 rewrite_branches
 rm .git/info/grafts
+
+# Convert all branches representing SVN tags into GIT annotated tags.
+convert_svn_branches_to_tags
 
 # Prune all unnecessary data from the repository.
 git gc --prune=now
